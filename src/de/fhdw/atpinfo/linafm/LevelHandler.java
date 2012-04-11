3dd916ee989f7c095fe0e8def0a15d27df9c1729
Brueggus <brueggus@orangevir.us>
2012-04-11 18:42:25 +0200

    Basis-Implementierung des Levelauswahlmenüs.

---
+++ src/de/fhdw/atpinfo/linafm/LevelHandler.java
@@ -0,0 +1,13 @@
+package de.fhdw.atpinfo.linafm;
+
+public class LevelHandler {
+	public static String[] getLevelCaptionsForMenu()
+	{
+		String[] tempCaptions = new String[] { 
+				"Blumenwiese", "Bauernhof", "Am Flughafen", "In der Weihnachtsbäckerei"
+		};
+		
+		return tempCaptions;
+	}
+		
+}
--- src/de/fhdw/atpinfo/linafm/LinafmActivity.java
+++ src/de/fhdw/atpinfo/linafm/LinafmActivity.java
@@ -2,41 +2,63 @@ package de.fhdw.atpinfo.linafm;
 
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ListView;
 
 public class LinafmActivity extends Activity implements OnClickListener {
 	
-	private Button play;
-	private Button end;
+	private Context context;
+	private Button mBtnPlay, mBtnEnd;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        play = (Button)findViewById(R.id.play);
-        end = (Button)findViewById(R.id.end);
-        play.setOnClickListener(this);
-        end.setOnClickListener(this);
+        mBtnPlay = (Button)findViewById(R.id.play);
+        mBtnEnd = (Button)findViewById(R.id.end);
+        mBtnPlay.setOnClickListener(this);
+        mBtnEnd.setOnClickListener(this);
     }
     
 	@Override
 	public void onClick(View button) {
 		switch (button.getId()) {
 		case R.id.play:
-			// Hier passiert etwas...
-			Context context = getApplicationContext();
-			Toast toast = Toast.makeText(context, "klick, klack... nichts passiert.", Toast.LENGTH_SHORT);
-			toast.show();			
+			openLevelChooserDialog();
 			break;
 		case R.id.end:
 			finish();
 			break;
 		}
+	}
+
+	private void openLevelChooserDialog() {
+		context = LinafmActivity.this;
+        final Dialog dialog = new Dialog(context);
+        dialog.setContentView(R.layout.levelchooser);
+        dialog.setTitle(R.string.choose_level);
+        dialog.setCancelable(true);
+        
+        ListView lv = (ListView) dialog.findViewById(R.id.lvLvl);
+        String[] levelNames = LevelHandler.getLevelCaptionsForMenu();
+        ArrayAdapter<String> menuItems = new ArrayAdapter<String>(this, R.layout.menu_item, levelNames);
+        lv.setAdapter(menuItems);
+
+        Button button = (Button) dialog.findViewById(R.id.btnCancel);
+        button.setOnClickListener(new OnClickListener() {
+        @Override
+            public void onClick(View v) {
+                dialog.cancel();
+            }
+        });
+   
+        dialog.show();
 	}
 }
