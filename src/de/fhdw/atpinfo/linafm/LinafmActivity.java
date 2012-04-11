3e0ba5da7fd46e13d33ce5ec35caeee8b63d2f86
Alexander Brügmann <mail@abruegmann.eu>
2012-04-11 17:52:33 +0200

    * LinafmActivity als Default-Activity gesetzt * den Buttons Leben eingehaucht * kleinere Veränderungen am Layout

--- src/de/fhdw/atpinfo/linafm/LinafmActivity.java
+++ src/de/fhdw/atpinfo/linafm/LinafmActivity.java
@@ -2,30 +2,41 @@ package de.fhdw.atpinfo.linafm;
 
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LinafmActivity extends Activity implements OnClickListener {
 	
 	private Button play;
 	private Button end;
-    /** Called when the activity is first created. */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
         play = (Button)findViewById(R.id.play);
         end = (Button)findViewById(R.id.end);
         play.setOnClickListener(this);
         end.setOnClickListener(this);
     }
+    
 	@Override
 	public void onClick(View button) {
-		if (button.toString() == "end") {
-			
+		switch (button.getId()) {
+		case R.id.play:
+			// Hier passiert etwas...
+			Context context = getApplicationContext();
+			Toast toast = Toast.makeText(context, "klick, klack... nichts passiert.", Toast.LENGTH_SHORT);
+			toast.show();			
+			break;
+		case R.id.end:
+			finish();
+			break;
 		}
-		
 	}
 }
